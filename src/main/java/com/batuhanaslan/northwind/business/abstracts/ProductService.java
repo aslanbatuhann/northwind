6a72359557ad9cd91efcada74ee61b3095a10fef
Batuhan Aslan <batuhanaslan9601@gmail.com>
2022-11-19 13:24:19 +0300

    business layer for the product

--- src/main/java/com/batuhanaslan/northwind/business/abstracts/ProductService.java
+++ src/main/java/com/batuhanaslan/northwind/business/abstracts/ProductService.java
@@ -3,6 +3,7 @@ package com.batuhanaslan.northwind.business.abstracts;
 import com.batuhanaslan.northwind.core.utilities.results.DataResult;
 import com.batuhanaslan.northwind.core.utilities.results.Result;
 import com.batuhanaslan.northwind.entities.concretes.Product;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +11,17 @@ public interface ProductService {
     DataResult<List<Product>> getAll();
     Result add(Product product);
 
+    DataResult<Product> getByProductName(String productName);
+
+    DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId);
+
+    DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId);
+
+    DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+
+    DataResult<List<Product>> getByProductNameContains(String productName);
+
+    DataResult<List<Product>> getByProductNameStartsWith(String productName);
+
+    DataResult<List<Product>> getByNameAndCategory(String productName,int categoryId);
 }
